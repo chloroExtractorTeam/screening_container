c02bccb00d354165a0e5f06d31e782547fc3958d
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-07-11 18:24:17 +0200

    Add a runner script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ACCESSION=${1}
+CPUCORES=${2:-16}
+RANDOMSEED=${3:-556854669}
+
+echo "ACCESSION: ${ACCESSION}, RANDOMSEED: ${RANDOMSEED}, CPUCORES: ${CPUCORES}"
+
+export LC_ALL="C.UTF-8"
+
+time fastq-dump --outdir ${ACCESSION} --split-files -v ${ACCESSION}
+
+cd ${ACCESSION}
+time fastq-shuffle.pl --randomseed ${RANDOMSEED} -1 ${ACCESSION}_1.fastq -2 ${ACCESSION}_2.fastq
+
+time ptx --threads ${CPUCORES} -1 ${ACCESSION}_1.fastq.shuffled -2 ${ACCESSION}_2.fastq.shuffled -d CE_${ACCESSION}_out
+
+time fast-plast.pl -1 ${ACCESSION}_1.fastq.shuffled -2 ${ACCESSION}_2.fastq.shuffled --name FP_${ACCESSION}_out --coverage_analysis --threads ${CPUCORES}
